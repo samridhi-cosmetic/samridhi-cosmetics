8b6fcce52ecbd52bd2d014ccf5e3fe4ecd40f318
samridhi-cosmetic <sgaurav857@gmail.com>
2026-01-12 17:28:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const products = [
     price: 499,
     featured: false,
     variants: [
-      {shade: "Black", img: "images/blueHeavenMascara.jpeg"}
+      {shade: "Black", img: "image/blueHeavenMascara.jpeg"}
     ]
   }
 ];
