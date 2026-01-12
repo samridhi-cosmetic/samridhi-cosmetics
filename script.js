cd91547f5312e75e042bf856f92dcd32b9f77b40
samridhi-cosmetic <sgaurav857@gmail.com>
2026-01-12 14:14:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function searchProducts() {
+let input = document.getElementById("search").value.toLowerCase();
+let products = document.getElementsByClassName("product");
+
+for (let i = 0; i < products.length; i++) {
+let name = products[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+products[i].style.display = name.includes(input) ? "block" : "none";
+}
+}
+
+function filter(category) {
+let products = document.getElementsByClassName("product");
+
+for (let i = 0; i < products.length; i++) {
+if (category == "all" || products[i].classList.contains(category))
+products[i].style.display = "block";
+else
+products[i].style.display = "none";
+}
+}
