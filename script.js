4047bb568769d937097297814880ebcb6e93c11e
samridhi-cosmetic <sgaurav857@gmail.com>
2026-01-12 14:27:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-function searchProducts() {
-let input = document.getElementById("search").value.toLowerCase();
-let products = document.getElementsByClassName("product");
-
-for (let i = 0; i < products.length; i++) {
-let name = products[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
-products[i].style.display = name.includes(input) ? "block" : "none";
-}
-}
-
 function filter(category) {
-let products = document.getElementsByClassName("product");
-
-for (let i = 0; i < products.length; i++) {
-if (category == "all" || products[i].classList.contains(category))
-products[i].style.display = "block";
-else
-products[i].style.display = "none";
+  let products = document.getElementsByClassName("product");
+  for (let i = 0; i < products.length; i++) {
+    if (category === "all" || products[i].classList.contains(category)) {
+      products[i].style.display = "block";
+    } else {
+      products[i].style.display = "none";
+    }
+  }
 }
+
+function searchProducts() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let products = document.getElementsByClassName("product");
+  for (let i = 0; i < products.length; i++) {
+    let name = products[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+    products[i].style.display = name.includes(input) ? "block" : "none";
+  }
 }
